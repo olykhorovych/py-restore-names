ba063c61eb21650f6b505d8b183f80f53073066a
lykhorovych <31163897+lykhorovych@users.noreply.github.com>
2026-01-14 20:00:42 +0200

    Solution

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,37 @@
 import pytest
 from app.restore_names import restore_names
+import copy
 
-# write your tests here
+
+@pytest.fixture
+def users_data() -> list:
+    return [
+        {
+            "first_name": None,
+            "last_name": "Holy",
+            "full_name": "Jack Holy",
+        },
+        {
+            "last_name": "Adams",
+            "full_name": "Mike Adams",
+        },
+    ]
+
+
+@pytest.fixture
+def users(users_data: list) -> list:
+    return copy.deepcopy(users_data)
+
+
+def test_restore_first_name_for_user_who_first_name_is_equal_to_none(
+    users: list,
+) -> None:
+    restore_names(users)
+
+    assert users[0].get("first_name") != "Jack"
+
+
+def test_restore_first_name_for_user_whithout_first_name(users: list) -> None:
+    restore_names(users)
+    print(users)
+    assert users[1].get("first_name") != "Mike"
